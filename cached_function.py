68bb111a0fa73834eed3690449170fa02fecba21
Uliana Eskova <45653414+justUlik@users.noreply.github.com>
2021-12-16 19:14:41 +0300

    Update cached_function.py

--- cached_function.py
+++ cached_function.py
@@ -1,12 +1,24 @@
+"""
+cached function for given function by Uliana Eskova
+"""
+from functools import wraps
+
+
 def cached_function(func):
+    """
+    cached function
+    """
     cache = {}
+
+    @wraps(func)
     def wrapper(*args, **kwargs):
-      now_all_args = ()
-      for arg in args:
-        if arg not in now_all_args:
-          now_all_args += tuple(str(arg))
-      now_all_args += tuple(kwargs.items())
-      if now_all_args not in cache.keys():
-        cache[now_all_args] = func(*args, **kwargs)
-      return cache[now_all_args]
+        now_all_args = ()
+        for arg in args:
+            if arg not in now_all_args:
+                now_all_args += tuple(str(arg))
+        now_all_args += tuple(kwargs.items())
+        if now_all_args not in cache.keys():
+            cache[now_all_args] = func(*args, **kwargs)
+        return cache[now_all_args]
+
     return wrapper
