ac7a6771ffaa1b5d95da19071c59cf0e8b82c4ee
Uliana Eskova <45653414+justUlik@users.noreply.github.com>
2022-01-24 21:45:56 +0300

    Update cached_function.py

--- cached_function.py
+++ cached_function.py
@@ -1,7 +1,37 @@
 """
-cached function for given function by Uliana Eskova
+cached function and lru cached function for given function by Uliana Eskova
 """
 from functools import wraps
+from collections import OrderedDict
+
+
+def lru_cached_function(cache_size):
+    """
+    lru cached function
+    """
+    if type(cache_size) != int:
+        raise TypeError('cache_size must be int not {}'.format(type(cache_size).__name__))
+    if cache_size <= 0:
+        raise TypeError('cache_size must be positive')
+    cache = OrderedDict()
+
+    def wrapper(func):
+        @wraps(func)
+        def called(*args, **kwargs):
+            now_all_args = ()
+            for arg in args:
+                if arg not in now_all_args:
+                    now_all_args += tuple(str(arg))
+            now_all_args += tuple(set(kwargs.items()))
+            try:
+                cache.move_to_end(now_all_args)
+            except KeyError:
+                if len(cache) == cache_size:
+                    cache.popitem(last=False)
+                cache[now_all_args] = func(*args, **kwargs)
+            return cache[now_all_args]
+        return called
+    return wrapper
 
 
 def cached_function(func):
